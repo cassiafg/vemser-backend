fc124a00a515f50bcaa66a41faa89324d82cf153
cassiafg <cassiafguimaraes@gmail.com>
2022-07-11 14:31:38 -0300

    Homework 5 (Correção lombok classes DTO)

--- modulo 3/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/dto/ContatoDTO.java
+++ modulo 3/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/dto/ContatoDTO.java
@@ -1,5 +1,8 @@
 package br.com.dbc.vemser.pessoaapi.dto;
 
+import lombok.Data;
+
+@Data
 public class ContatoDTO extends ContatoCreateDTO{
     private Integer idContato;
 }
--- modulo 3/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/dto/PessoaDTO.java
+++ modulo 3/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/dto/PessoaDTO.java
@@ -1,5 +1,8 @@
 package br.com.dbc.vemser.pessoaapi.dto;
 
+import lombok.Data;
+
+@Data
 public class PessoaDTO extends PessoaCreateDTO{
     private Integer idPessoa;
 }
