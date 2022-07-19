0478b1ae8c2c3c380039cc7ad89db0360bb4b1ea
cassiafg <cassiafguimaraes@gmail.com>
2022-07-19 20:57:15 -0300

    Homework1 corrigido (@Repository no ContatoRepository)

--- modulo3/modulo3.2/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/repository/ContatoRepository.java
+++ modulo3/modulo3.2/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/repository/ContatoRepository.java
@@ -2,6 +2,8 @@ package br.com.dbc.vemser.pessoaapi.repository;
 
 import br.com.dbc.vemser.pessoaapi.entity.ContatoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContatoRepository extends JpaRepository<ContatoEntity, Integer> {
 }
