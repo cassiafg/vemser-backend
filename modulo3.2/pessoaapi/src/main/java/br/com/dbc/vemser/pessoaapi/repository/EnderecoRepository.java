1a997b98f865c460e14be23f03408e021c840548
cassiafg <cassiafguimaraes@gmail.com>
2022-07-21 16:37:54 -0300

    Exercício#1 Aula4

--- modulo3.2/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/repository/EnderecoRepository.java
+++ modulo3.2/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/repository/EnderecoRepository.java
@@ -1,6 +1,8 @@
 package br.com.dbc.vemser.pessoaapi.repository;
 
 import br.com.dbc.vemser.pessoaapi.entity.EnderecoEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,9 +12,10 @@ import java.util.List;
 
 @Repository
 public interface EnderecoRepository extends JpaRepository<EnderecoEntity, Integer> {
-//    @Query(" select e " +
-//            " from ENDERECO e" +
-//            " where e.pais = :pais"
-//    )
-//    List<EnderecoEntity> enderecoByPais(@Param("pais") String pais);
+
+        @Query(" select e " +
+            " from endereco_pessoa e" +
+            " where e.pais = :pais"
+    )
+        Page<EnderecoEntity> enderecoByPais(Pageable pageable, @Param("pais") String pais);
 }
